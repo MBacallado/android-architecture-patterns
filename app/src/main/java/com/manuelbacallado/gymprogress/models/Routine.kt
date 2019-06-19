04ee46d9c4ac5c0a458de8282ab9ad92c852fd9e
Manuel Bacallado <manuelbacallado89@gmail.com>
2019-06-19 18:02:01 +0100

    some layouts xml resources added

--- app/src/main/java/com/manuelbacallado/gymprogress/models/Routine.kt
+++ app/src/main/java/com/manuelbacallado/gymprogress/models/Routine.kt
@@ -1,2 +1,6 @@
 package com.manuelbacallado.gymprogress.models
 
+import java.util.*
+
+data class Routine(var name : String, var startDate : Date, var finishDate : Date,
+                   var trainingDays : Int, var trainingTypes : String)
